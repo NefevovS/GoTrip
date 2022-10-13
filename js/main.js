b4d73e3b4721598026bf80726f07fe9fa988af87
Nefedov Sergey <nefedov38@gmail.com>
2022-10-13 15:10:49 +0300

    123

--- js/main.js
+++ js/main.js
@@ -1,8 +1,19 @@
-$(document).ready(function () {
-  $(".owl-carousel").owlCarousel({
-    center: true,
-    loop: true,
-    margin: 30,
-    startPosition: 1,
-  });
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  center: true,
+  loop: true,
+  margin: 30,
+  startPosition: 1,
+});
+
+// Go to the previous item
+$(".slider__btn--prev").click(function () {
+  // With optional speed parameter
+  // Parameters has to be in square bracket '[]'
+  owl.trigger("prev.owl.carousel");
+});
+
+// Go to the next item
+$(".slider__btn--next").click(function () {
+  owl.trigger("next.owl.carousel");
 });
